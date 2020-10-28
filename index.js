1efdca9e6db858ee5309281325a787b3db5509d3
J2b2590 <J2b2590@gmail.com>
2020-10-28 12:00:47 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(obj){
+            console.log(obj)
+            document.body.innerHTML = obj["id"]
+        })
+        .catch(function(error) {
+            alert("Unauthorized Access");
+            console.log(error.message);
+            document.body.innerHTML = error
+          });       
+
+}
